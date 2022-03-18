1d3c1c6222ca545a0eac30675082cfe6ba85703d
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2022-03-18 13:47:45 +0600

    create-react-app

---
+++ save-the-date/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SaveTheDate : A Party-planning Application</h1>
+    </div>
+  );
+}
+
+export default App;
