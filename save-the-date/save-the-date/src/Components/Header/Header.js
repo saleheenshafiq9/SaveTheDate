7c784ece089984b304407610e4dd20fa9718bf69
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2022-04-12 02:29:11 +0600

    Creating Navigation Bar

--- save-the-date/save-the-date/src/Components/Header/Header.js
+++ save-the-date/save-the-date/src/Components/Header/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./Header.css";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import Logo from "../../assets/mirror-ball.png";
 
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
+    <div className="Navigation">
+      <Navbar
+        style={{
+          backgroundColor: "#2EC4B6",
+          height: "75px",
+        }}
+      >
+        <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+          <img src={Logo} alt="Logo" width="40px" />
+          SaveTheDate
+        </NavbarBrand>
+        <Nav className="mr-md-5">
+          <NavItem>
+            <NavLink href="#" className="NavLink">
+              Something
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 };
