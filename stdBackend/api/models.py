dd74e71c1574f093e6900f1b539e5f6dd92cc872
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-04-17 01:48:40 +0600

    Created Customer model, Updated User Model

--- stdBackend/api/models.py
+++ stdBackend/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    birthDate=models.DateField(null=True, blank=True)
+    gender=models.CharField(max_length=255)
