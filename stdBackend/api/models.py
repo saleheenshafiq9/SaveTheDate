750c0ff84871a890c8c29bb7a121dc586fdedcff
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-04-17 02:14:17 +0600

    Creating Data models

--- stdBackend/api/models.py
+++ stdBackend/api/models.py
@@ -4,3 +4,33 @@ from django.db import models
 class Customer(models.Model):
     birthDate=models.DateField(null=True, blank=True)
     gender=models.CharField(max_length=255)
+
+class ServiceProvider(models.Model):
+    description=models.TextField(null=True, blank=True)
+
+class Venue(ServiceProvider):
+    pass
+
+class Catering(ServiceProvider):
+    pass
+
+class Decorator(ServiceProvider):
+    pass
+
+class ContentMaker(ServiceProvider):
+    pass
+
+class Entertainer(ServiceProvider):
+    pass
+
+class Appointment(models.Model):
+    Category=models.CharField(max_length=255)
+    ScheduledAt=models.DateTimeField()
+
+class Party(models.Model):
+    pass
+
+class Notification(models.Model):
+    notifiedAt=models.DateTimeField()
+    mobileNumber=models.CharField(max_length=255)
+    
--- stdBackend/stdBackend/settings.py
+++ stdBackend/stdBackend/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'stdBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'savethedate',
+        'HOST':'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
