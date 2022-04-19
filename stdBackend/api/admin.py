3d6aafc1000296f9ed8c849197bba8b8c39144c5
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-04-19 11:55:30 +0600

    Admin Interface

--- stdBackend/api/admin.py
+++ stdBackend/api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .import models
+    
 
 # Register your models here.
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Venue)
+class VenueAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Catering)
+class CateringAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Decorator)
+class DecoratorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ContentMaker)
+class ContentMakerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Entertainer)
+class EntertainerAdmin(admin.ModelAdmin):
+    pass
