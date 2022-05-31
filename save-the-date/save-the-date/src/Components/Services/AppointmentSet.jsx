84e372b6b8c2fca580e9b2dc8b5ede9c8183a24f
Shafiq Saleheen <bsse1125@iit.du.ac.bd>
2022-05-31 07:16:11 +0600

    appointment done

--- save-the-date/save-the-date/src/Components/Services/AppointmentSet.jsx
+++ save-the-date/save-the-date/src/Components/Services/AppointmentSet.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Schedule.css";
 
 export default function AppointmentSet(props) {
+    const [confirm, setConfirm] = useState(false);
+
+    const ClickConfirm = () => {
+        setConfirm(true);
+    }
 
   return (
-    <div>
-        <a className='text-center'><br/><h6 className='text-center'>{props.confirm}</h6></a>
+    <div onClick={ClickConfirm}>
+        { confirm? <h6 className='text-center'>Done!</h6> : <a className='text-center'><br/><h6 className='text-center'>{props.confirm}</h6></a> }
     </div>
   )
 }
